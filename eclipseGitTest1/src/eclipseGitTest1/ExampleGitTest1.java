39a6b467707e73b0f1dd114e2b55cec3a3415e1f
Dalton Gorman <djgotown@gmail.com>
2020-02-24 10:52:23 -0500

    Multi Project Test

---
+++ eclipseGitTest1/src/eclipseGitTest1/ExampleGitTest1.java
@@ -0,0 +1,11 @@
+package eclipseGitTest1;
+
+public class ExampleGitTest1 {
+
+	public static void main(String[] args) {
+		//This is equivalent to problem_1
+		System.out.println("I hope this works");
+
+	}
+
+}
---
+++ eclipseGitTest2/src/eclipseGitTest2/ExampleGitTest2Class1.java
@@ -0,0 +1,11 @@
+package eclipseGitTest2;
+
+public class ExampleGitTest2Class1 {
+
+	public static void main(String[] args) {
+		//This is equivalent to problem_2
+		System.out.println("This is equivalent to problem_2");
+
+	}
+
+}
